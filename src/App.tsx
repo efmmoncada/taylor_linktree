0c0eb7dbb789238fa67a5eeca75905b9de80b9af
efmmoncada <emmanuel.moncada27@gmail.com>
2024-02-23 19:14:45 -0800

    fix layout shift on load
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,61 @@
-import moon from "./moon.jpg";
+import { useEffect, useRef, useState } from "react";
 import { links } from "../data";
 import Link from "./Link";
-import { useEffect, useRef, useState } from "react";
+import moon from "./moon.jpg";
 
 function App() {
   const ref = useRef<HTMLImageElement>(null);
-  const [maxWidth, setMaxWidth] = useState<number>();
+  const [maxWidth, setMaxWidth] = useState<number>(0);
 
   useEffect(() => {
     setTimeout(() => {
       setMaxWidth(ref.current ? ref.current.offsetWidth : 0);
-    }, 500);
-  }, []);
+    }, 250);
+  }, [ref.current]);
 
   return (
-    <div
-      className="text-center font-main relative mx-auto text-white h-full w-full"
-      style={{
-        maxWidth: maxWidth,
-      }}
-    >
+    <>
       <img
         ref={ref}
-        className="opacity-75 h-full object-cover object-center absolute"
+        className="opacity-75 h-full mx-auto object-cover object-center"
         src={moon}
         alt="moon"
       />
+
       <div
-        className="relative h-1/2"
+        className="text-center w-full font-main inset-0 mx-auto absolute transition-opacity text-white h-full"
         style={{
           maxWidth: maxWidth,
+          opacity: maxWidth === 0 ? 0 : 1,
         }}
       >
-        <h1 className="text-4xl font-extralight top-14 left-8 absolute short:text-3xl">
-          t a y l o r
-        </h1>
-        <h1 className="text-4xl font-extralight absolute right-8 bottom-11 short:text-3xl">
-          l a n d r e s s
-        </h1>
-      </div>
-      <div
-        className="h-1/2 flex justify-center items-center relative"
-        style={{
-          maxWidth: maxWidth,
-        }}
-      >
-        <div className="grid gap-4 justify-center z-20">
-          {links.map((link, i) => (
-            <Link key={i} {...link} />
-          ))}
+        <div
+          className="relative h-1/2"
+          style={{
+            maxWidth: maxWidth,
+          }}
+        >
+          <h1 className="text-4xl font-extralight top-14 left-8 absolute short:text-3xl">
+            t a y l o r
+          </h1>
+          <h1 className="text-4xl font-extralight absolute right-8 bottom-11 short:text-3xl">
+            l a n d r e s s
+          </h1>
+        </div>
+        <div
+          className="h-1/2 flex justify-center items-center relative"
+          style={{
+            maxWidth: maxWidth,
+          }}
+        >
+          <div className="grid gap-4 justify-center z-20">
+            {links.map((link, i) => (
+              <Link key={i} {...link} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
